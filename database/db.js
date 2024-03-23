e20824dbfb642845220656494188b05498c52dfb
Somkene Ojukwu <somkeneoj@gmail.com>
2024-03-23 15:17:36 +0100

    adding .env file

--- database/db.js
+++ database/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost', // or your PostgreSQL server's host
+    user: `${process.env.USERNAME}`,
+    host: `${process.env.HOST}`, // or your PostgreSQL server's host
     database: 'test',
-    password: 'muscle$$090',
-    port: 5432, // or your PostgreSQL server's port
+    password: `${process.env.PASSWORD}`,
+    port: process.env.DBPORT, // or your PostgreSQL server's port
 });
 
 module.exports = pool;
